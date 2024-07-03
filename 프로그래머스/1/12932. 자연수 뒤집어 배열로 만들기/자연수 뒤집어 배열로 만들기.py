bfd21ff0ed6b5ebcaab8efe59fd791df74db2113
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-07-03 17:56:29 +0900

    자연수 뒤집어 배열로 만들기.py

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -1,7 +1,9 @@
+# reversed 사용
 def solution(n):
     answer = list(map(int, reversed(str(n))))
     return answer
 
+# 슬라이싱 역순 사용
 def solution2(n):
     answer = list(map(int, (str(n)[::-1])))
     return answer
