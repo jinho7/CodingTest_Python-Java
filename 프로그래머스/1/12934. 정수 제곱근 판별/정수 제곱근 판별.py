a6ed4513bfbb9a412be06a51839ada5c4dcabb25
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-07-03 19:25:36 +0900

    정수 제곱근 판별.py

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -7,3 +7,12 @@ def solution(n):
         return (n_sqrt + 1) * (n_sqrt + 1)
     else :
         return -1
+
+# impprt math 없이 코드 짜기 
+def solution2(n):
+    sqrt = n ** (1/2)
+
+    if sqrt % 1 == 0:
+        return (sqrt + 1) ** 2
+    else :
+        return -1
