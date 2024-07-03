488825ea3f2a400b91f024147de0bde764e34a3d
jinho7 <69239293+jinho7@users.noreply.github.com>
2024-07-03 19:03:09 +0900

    [level 1] Title: 정수 내림차순으로 배치하기 ; sort와 reverse를 이용한 풀이

---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기 copy.py
@@ -0,0 +1,15 @@
+# sort와 reverse를 이용한 풀이
+
+# (1) .sort
+# 리스트를 정렬하는 파이썬의 내장 함수
+# reverse = True: 내림차순 정렬
+# reverse = False: 오름차순 정렬 (기본값)
+# key: 정렬 기준을 지정
+# ex) key=len : 문자열 길이를 기준으로 정렬
+
+# "".join(ls): 정렬된 리스트의 모든 요소를 빈 문자열("")을 구분자로 사용하여 하나의 문자열로 결합
+
+def solution(n):
+    ls = list(str(n))
+    ls.sort(reverse = True)
+    return int("".join(ls))
