86b8a472a82a92fff5a151a58e0d2eefb55cabb5
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-07-08 02:41:45 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -3,3 +3,8 @@ def solution(price, money, count):
     for i in range(price, price*count+1, price):
         sum+=i
     return sum - money if money < sum else 0
+
+# return max(0,price*(count+1)*count//2-money)
+# 개섹시하다. 
+# 등차 수열의 합 공식 활용..
+# max() 로 음수나오면 (돈 안부족하면) 0 뽑히게..
