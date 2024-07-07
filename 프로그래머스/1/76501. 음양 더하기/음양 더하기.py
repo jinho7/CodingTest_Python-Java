9b9ba269857e27e82d2a1a293edadc406c36ad76
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-07-07 19:59:18 +0900

    [level 1] Title: 음양 더하기, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -4,3 +4,14 @@ def solution(absolutes, signs):
         if not item :
             absolutes[i] *= -1
     return sum(absolutes)
+
+# zip을 이용한 풀이
+# 여러 개의 이터러블(iterable, 반복 가능한 객체)을 인자로 받아 각 이터러블의 요소들을 튜플로 묶어 반환
+def solution2(absolutes, signs):
+    answer=0
+    for absolute,sign in zip(absolutes,signs):
+        if sign:
+            answer+=absolute
+        else:
+            answer-=absolute
+    return answer
