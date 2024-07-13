50637f750601ea07e86d4a3e55d9d646fa59e586
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-07-13 13:04:19 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -4,4 +4,4 @@ def solution(food):
         food[i] = (food[i]//2)*2
     for i in range(1, len(food)):
         answer += str(i)*(food[i]//2)
-    return answer + '0' + ''.join(sorted(list(answer), reverse=True))
+    return answer + '0' + answer[::-1]
