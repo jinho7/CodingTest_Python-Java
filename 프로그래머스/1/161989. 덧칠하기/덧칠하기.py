ac472ee484bbb60d19883b21e6b56000321a1988
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-07-15 00:46:52 +0900

    [level 1] Title: 덧칠하기, Time: 4.41 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,4 +1,4 @@
-def solution(n, m, section):
+def my_solution(n, m, section):
     bit = list("1" * n)
     for i in range(len(section)):
         bit[section[i]-1] = "0"
@@ -19,5 +19,21 @@ def solution(n, m, section):
             # m칸칸큼 1로 바꿈
             bit[i:i+m] = ["1"] * m
             count += 1
+    
+    return count
+
+
+def solution(n, m, section):
+    count = 0
+    painted = 0
+    
+    # 안칠해진 영역 모두 돌기.
+    for s in section:
+        # 0 보다 크면 (처음에는 무조건 만남)
+        # 여기 안걸리면 이전에 칠해버린거
+        if s > painted:
+            count += 1
+            # 여기까진 칠해졌다! -> 섹션 + 페인트 -1
+            painted = s + m - 1
     
     return count
