fd9505fc4b6bf6525417cf10809133995066f89c
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-07-15 13:47:43 +0900

    [level 1] Title: 콜라 문제, Time: 0.36 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -5,3 +5,15 @@ def solution(a, b, n):
         total_bottles += exchanged
         n = exchanged + (n % a)
     return total_bottles
+
+
+total_bottles = 0
+def my_solution(a, b, n):
+    global total_bottles
+    if n >= a:
+        exchanged = (n // a) * b
+        total_bottles += exchanged
+        n = exchanged + (n % a)
+        return solution(a, b, n)
+    else:
+        return total_bottles
