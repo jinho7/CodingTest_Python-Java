0fd3cca31912044f40aec7119dc0ba1e5b932477
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-07-17 16:50:47 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,10 +1,10 @@
 # [level 1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484#fnref1) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.02 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 17일 16:40:29
+2024년 07월 17일 16:50:44
 
 ### 문제 설명
 
--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,37 +1,14 @@
-# 최저 / 최대 순위 구하기
 def solution(lottos, win_nums):
     
-    lottos.sort()
-    win_nums.sort()
-    caculate(lottos, win_nums)
-    max_correct = caculate(lottos, win_nums)[0]+caculate(lottos, win_nums)[2]
-    min_correct = caculate(lottos, win_nums)[0]
-    return [rank(max_correct), rank(min_correct)]
-
-    # for i in range(len(lottos)):
-    #     if lottos[i]==0:
-    #         print("")
-    # return 
-
-def caculate(arr1, answer):
-    answer1 = answer.copy()
+    zero = lottos.count(0)
     correct = 0
-    uncorrect = 0
-    zero = 0
     # 역순으로 순회
-    for a in range(len(arr1)-1, -1, -1):
-        if arr1[a] in answer1:
-            answer1.pop(a)
+    for a in range(len(lottos)):
+        if lottos[a] in win_nums:
             correct += 1
-        elif arr1[a] not in answer1:
-            if arr1[a] == 0:
-                zero += 1
-            else:
-                uncorrect += 1
-    return correct, uncorrect, zero
-
-def rank(int):
-    if int == 0:
-        return 6
-    else:
-        return 7-int
+            
+    max_ = correct + zero
+    min_ = correct
+    max_rank = 7- max_ if max_ >=1 else 6
+    min_rank = 7- min_ if min_ >=1 else 6
+    return [max_rank, min_rank]
