2b43c1bfca9ee4419c2103c195b02eb6d9bb3da7
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-07-20 17:59:03 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
@@ -1,3 +1,19 @@
 def solution(board, h, w):
-    answer = 0
-    return answer
+    # board의 길이와 board[?]의 길이는 동일 -> n x n의 2차원 격자 판
+    n = len(board)
+    
+    # 같은 색으로 색칠된 칸의 개수를 저장할 변수
+    count = 0
+    
+    # 이동을 의미 (dh와 dw를 조합하여 0,1 / 1,0 / -1,0 / 0,1 을 모두 만들 수 있음)
+    dh = [0, 1, -1, 0]
+    dw = [1, 0, 0, -1]
+    
+    
+    for i in range(4):
+        h_check, w_check = h + dh[i], w + dw[i]
+        if 0 <= h_check < n and 0 <= w_check < n:
+            if board[h][w] == board[h_check][w_check]:
+                count += 1
+    
+    return count
