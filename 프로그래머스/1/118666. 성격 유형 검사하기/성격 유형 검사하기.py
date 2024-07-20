aacabc6251f5d4b506b5daff566344c986688a23
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-07-21 00:30:29 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,6 +1,7 @@
 def solution(survey, choices):
     dic = {'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0}
     
+    # 4를 기준으로 [0] 혹은 [1] 에 해당하는 key의 value에 점수를 저장
     for i in range(len(choices)):
         if choices[i] < 4:
             dic[survey[i][0]] += 4 - choices[i]
@@ -8,6 +9,8 @@ def solution(survey, choices):
             dic[survey[i][1]] += choices[i] - 4
     
     result = ''
+    
+    # 크거나 같다로 처리
     result += 'R' if dic['R'] >= dic['T'] else 'T'
     result += 'C' if dic['C'] >= dic['F'] else 'F'
     result += 'J' if dic['J'] >= dic['M'] else 'M'
