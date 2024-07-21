1ac3cd089202afe9fa63a13d2c52a6b0e3f7ba8c
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-07-21 20:24:57 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.24 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,24 +1,17 @@
 def solution(wallpaper):
-    
+
+    # x축 최대 최소 구하기
+    cand_x = []
+    for index in range(len(wallpaper)):
+        if '#' in wallpaper[index]:
+            cand_x.append(index)
     
     # y축 최소 최대 구하기
-    candidate = []
+    cand_y = []
     for line in wallpaper:
         for index, item in enumerate(line):
             # 처음 '#'을 만나면 후보에 index를 추가하고 멈춘다.
             if item == '#':
-                candidate.append(index)
-    # 그 중 제일 작은 것이 y축 최소값
-    luy = min(candidate)
-    # 그 중 제일 큰 것 +1 이 y축 최대값
-    rdy = max(candidate)+1
+                cand_y.append(index)
     
-    # x축 최대 최소 구하기
-    candidate = []
-    for index in range(len(wallpaper)):
-        if '#' in wallpaper[index]:
-            candidate.append(index)
-    lux = min(candidate)
-    rdx = max(candidate)+1
-    
-    return [lux, luy, rdx, rdy]
+    return [min(cand_x), min(cand_y), max(cand_x)+1, max(cand_y)+1]
