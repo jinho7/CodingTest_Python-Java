d9b2f93b91bbfdf8ca8dc5294158583007a23dec
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-07-24 06:38:51 +0900

    [level 2] Title: 피보나치 수, Time: 403.23 ms, Memory: 467 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -3,8 +3,10 @@
 fibo_dict = {'fibo0' : 0, 'fibo1' : 1}
 
 def solution(n):
+    # 한 번 1 부터 돌려서 딕셔너리에 저장
     for i in range(2, n):
         fibo(i)
+    # 실제 fibo(n) 은 사전에서 찾아와서 오래 안걸릴 듯
     return fibo(n) % 1234567
 
 def fibo(n):
