2c15b864888067887a15147cafc04d10be76d769
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-07-31 17:22:13 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4233.03 ms, Memory: 43.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,11 @@
+def solution(elements):
+    # 일단 mod를 써야겠다.
+    sum_set = set()
+    element = elements + elements
+    length = len(elements)
+    for i in range(1, length+1):
+        print("i", i)
+        for k in range(0, length):
+            #print("k", k, "k+i", k+i)
+            sum_set.add(sum(element[k: k+i]))
+    return len(sum_set)
