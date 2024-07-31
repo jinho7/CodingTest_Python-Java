380b20cfab76c4e9124387fbe7dc0155c8ca0d72
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-07-31 17:23:47 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4215.61 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,10 +1,12 @@
 def solution(elements):
     # 일단 mod를 써야겠다.
+    # 그냥 두배로 늘려..
     sum_set = set()
     element = elements + elements
     length = len(elements)
+    
     for i in range(1, length+1):
-        print("i", i)
+        #print("i", i)
         for k in range(0, length):
             #print("k", k, "k+i", k+i)
             sum_set.add(sum(element[k: k+i]))
