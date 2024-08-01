384db15738dc7a30661c130530758118cc5f799a
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-08-01 14:25:26 +0900

    [level 2] Title: 귤 고르기, Time: 47.29 ms, Memory: 27.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -8,7 +8,7 @@ def solution(k, tangerine):
         size_count_dict[i] += 1 # 딕셔너리에 값 생성, 저장
     # 뭐 실제 크기가 중요한 것은 아님. 그냥 몇가지 종류로 나눌 수 있는지가 중요.
     # key는 날리자
-    counts = sorted((i for i in size_count_dict.values()), reverse = True) 
+    counts = sorted(size_count_dict.values(), reverse = True) 
     # 큰 수부터 개수만 남음
     
     for index, count in enumerate(counts): # 인덱스와 같이 for문으로 돌리기
