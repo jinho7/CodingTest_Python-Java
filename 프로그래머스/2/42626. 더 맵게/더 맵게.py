1eeeaca566b0b97a9d907adeb13ba79ae9770daa
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-08-08 08:38:16 +0900

    [level 2] Title: 더 맵게, Time: 1650.44 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,24 @@
+# 힙 사용 안하면 못 풀겠다 싶어서 조금 참고함.
+import heapq
+
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    sum = 0
+    max = len(scoville)-1
+    
+    while not check_all_over(scoville, K):
+        if sum == max:
+            return -1
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        heapq.heappush(scoville, first + second * 2)
+        sum += 1
+        
+    return sum
+
+def check_all_over(scoville, K):
+    for food in scoville:
+        if food < K:
+            return False
+    return True
+    
