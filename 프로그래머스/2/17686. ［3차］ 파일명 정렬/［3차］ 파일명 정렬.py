96f3b55cb518fa7dbf8d89687bc6fec151ebb607
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-08-12 13:24:17 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 6.03 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,3 +1,17 @@
 def solution(files):
-    answer = []
-    return answer
+    def split_file(file):
+        head = ""
+        number = ""
+        tail = ""
+        i = 0
+        while i < len(file) and not file[i].isdigit():
+            head += file[i]
+            i += 1
+        while i < len(file) and file[i].isdigit() and len(number) < 5:
+            number += file[i]
+            i += 1
+        return (head, number)
+
+    files.sort(key=lambda file: (split_file(file)[0].lower(), int(split_file(file)[1])))
+
+    return files
