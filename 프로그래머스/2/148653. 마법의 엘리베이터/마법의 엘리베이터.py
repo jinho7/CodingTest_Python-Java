5c22c4a9937ee1e51f6f2ef5aec2b6e943e12f4e
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-08-19 03:56:01 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -0,0 +1,17 @@
+def solution(storey):
+    answer = 0
+    
+    while storey > 0:
+        i = storey % 10
+        storey = storey // 10
+
+        print (storey, i)
+
+        # 올림
+        if i > 5 or (i == 5 and storey % 10 >= 5):
+            storey += 1
+            answer += (10-i)
+        # 내림
+        else:
+            answer += i
+    return answer
