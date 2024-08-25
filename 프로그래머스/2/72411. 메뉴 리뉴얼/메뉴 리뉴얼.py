27e7590e7c4718fb8aabbe51a34b0de1d8318995
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-08-25 20:14:27 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 2.53 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -0,0 +1,25 @@
+from itertools import combinations
+from collections import defaultdict
+
+def solution(orders, course):
+    result = []
+    
+    for c in course:
+        freq_dict = defaultdict(int)
+        for order in orders:
+            # 오름차순 안하면, XY, YX 다르게 집계됨
+            for comb in combinations(sorted(list(order)), c):
+                a = ''.join(comb)
+                freq_dict[a] += 1
+                
+        # freq_dict 있다면,
+        if freq_dict:
+            max_value = max(freq_dict.values())
+            if max_value >= 2:
+                for key, value in freq_dict.items():
+                    if value == max_value:
+                        print(value)
+                        result.append(key)
+    return sorted(result)
+
+# from collections import defaultdict 암기
