2826a141fb00ec5cd396eabf2289db2e41b7df70
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-08-26 13:15:36 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 160.29 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -1,25 +1,35 @@
 import math
 
-def gcd_of_list(lst):
+def gcd_list(lst):
     gcd_result = lst[0]
     for i in range(1, len(lst)):
         gcd_result = math.gcd(gcd_result, lst[i])
     return gcd_result
 
 def solution(arrayA, arrayB):
-    gcd_A = gcd_of_list(arrayA)
-    gcd_B = gcd_of_list(arrayB)
+    gcd_A = gcd_list(arrayA)
+    gcd_B = gcd_list(arrayB)
+
+    result_A = 0
+    result_B = 0
     
-    # 조건 1: gcd_A가 arrayB의 모든 원소를 나누지 못하는지 확인
-    if all(b % gcd_A != 0 for b in arrayB):
+    # 조건 1: gcd_A가 arrayB의 모든 원소를 나눌 수 없는지 확인
+    is_valid_A = True
+    for b in arrayB:
+        if b % gcd_A == 0:
+            is_valid_A = False
+            break
+    if is_valid_A:
         result_A = gcd_A
-    else:
-        result_A = 0
     
-    # 조건 2: gcd_B가 arrayA의 모든 원소를 나누지 못하는지 확인
-    if all(a % gcd_B != 0 for a in arrayA):
+    # 조건 2: gcd_B가 arrayA의 모든 원소를 나눌 수 없는지 확인
+    is_valid_B = True
+    for a in arrayA:
+        if a % gcd_B == 0:
+            is_valid_B = False
+            break
+    if is_valid_B:
         result_B = gcd_B
-    else:
-        result_B = 0
     
+    # 두 값 중 큰 값을 반환
     return max(result_A, result_B)
