878fc4bfa8caccd728c2e8fa25044697f23bf681
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-09-04 21:39:36 +0900

    [level 2] Title: 리코쳇 로봇, Time: 13.99 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -6,43 +6,45 @@ def solution(board):
             if board[i][j] == 'R':
                 start_x, start_y = i, j
     
+    rows = len(board)
+    cols = len(board[0])
+        
+    # 방문 배열
+    visited = [[False for _ in range(cols)] for _ in range(rows)]
+    visited[start_x][start_y] = True
+    
     def bfs(start_x, start_y, board):
-        rows = len(board)
-        cols = len(board[0])
 
         # 방향 벡터 (상, 하, 좌, 우)
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
         # BFS를 위한 큐 초기화
-        queue = deque([(start_x, start_y, 0)])  # (x, y, 거리)
-        visited = [[False] * cols for _ in range(rows)]
-        visited[start_x][start_y] = True
-
+        queue = deque([(start_x, start_y, 0)])
+        
         while queue:
             x, y, distance = queue.popleft()
 
-            # 목표 지점에 도달한 경우
+            # G 도착
             if board[x][y] == "G":
                 return distance
 
-            # 네 방향으로 이동
+            # 네 방향 이동
             for dx, dy in directions:
                 next_x, next_y = x, y
 
-                # 다음에 벽을 만나거나, 장애물을 만날 때까지 이동
+                # 다음에 벽을 만나거나, 장애물을 만날 때까지
                 while True:
-                    # 다음 위치가 보드 범위를 벗어나거나, 장애물(D)을 만나면 중단
+                    # 다음 위치가 보드 범위를 벗어나거나, D을 만나면 break
                     if not (0 <= next_x + dx < rows and 0 <= next_y + dy < cols) or board[next_x + dx][next_y + dy] == "D":
                         break
                     next_x += dx
                     next_y += dy
 
-                # 방문하지 않은 위치라면 큐에 추가하고 방문 처리
+                # 방문하지 않은 위치 -> 큐에 추가 + 방문 처리
                 if not visited[next_x][next_y]:
                     visited[next_x][next_y] = True
                     queue.append((next_x, next_y, distance + 1))
         
-        # 목표 지점에 도달하지 못한 경우
         return -1
 
     answer = bfs(start_x, start_y, board)
