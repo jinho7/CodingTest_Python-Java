1f72aba2c0f976d67b5701cdd9ef6af765c76af4
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-10-12 21:26:42 +0900

    [level 2] Title: 배달, Time: 4.99 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,7 +1,43 @@
+import heapq
+from collections import defaultdict
+
 def solution(N, road, K):
-    answer = 0
 
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
-    return answer
+    graph = defaultdict(list)
+    
+    for start, end, weight in road:
+        graph[start].append((weight, end))
+        graph[end].append((weight, start))
+        
+    print(graph)
+    
+    # 1부터 각 마을의 최소 거리
+    distances = {node : float('inf') for node in range(1, N+1)}
+    distances[1] = 0
+    
+    # 우선순위 큐에 저장 (앞에 기준임 -> (weight, 연결 node) )
+    queue = [(0, 1)]
+    # heapq.heappush(), heapq.heappop() 으로 함수로만 적으면 힙정렬이 돼서 들어감
+    # 선언은 그냥 queue 처럼
+    
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+        
+        # 종료 or pass 조건 먼저
+        # 만약에 지금 가려는 게 저장된 것 보다 길다면 pass
+        if current_distance > distances[current_node]:
+            continue
+            
+        # 해당 노드에서 인접한 노드 모두 탐색
+        # 마치 dx, dy in direction 처럼 
+        for weight, next_node in graph[current_node]:
+            distance = current_distance + weight
+            
+            # 더 짧은 경로 발견 시 거리 테이블과 큐를 업데이트
+            if distance < distances[next_node]:
+                distances[next_node] = distance
+                heapq.heappush(queue, (distance, next_node))
+    
+    reachable_nodes = sum(1 for distance in distances.values() if distance <= K)
+        
+    return reachable_nodes
