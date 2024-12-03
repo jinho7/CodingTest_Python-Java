f0b040b2a5c7e8bc08ef7e37114504fb1782da49
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-12-03 13:41:06 +0900

    [level 3] Title: 기지국 설치, Time: 4.67 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -0,0 +1,24 @@
+import math
+
+def solution(n, stations, w):
+    answer = 0
+    unreachable = []
+    
+    # 첫 기지국까지의 처리
+    if stations[0]-w > 1:
+        unreachable.extend([1, stations[0]-w-1])
+    
+    # 기지국 간 간격 처리
+    for i in range(len(stations)-1):
+        if stations[i]+w+1 < stations[i+1]-w:
+            unreachable.extend([stations[i]+w+1, stations[i+1]-w-1])
+            
+    # 마지막 기지국 이후 처리
+    if stations[-1]+w < n:
+         unreachable.extend([stations[-1]+w+1, n])
+                
+    # 간격마다 필요한 기지국 수 계산
+    for j in range(0, len(unreachable), 2):
+        answer += math.ceil((unreachable[j+1] + 1 - unreachable[j]) / (2*w+1))
+        
+    return answer
