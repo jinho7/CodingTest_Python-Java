da29be647cd8a9f9daa67e5951f7dc05ebc72ed4
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-12-28 13:08:57 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.90 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -3,26 +3,31 @@ from collections import deque
 def solution(maps):
     n, m = len(maps), len(maps[0])
     
-    def bfs(x, y):
-        queue = deque([(x, y, 1)])
-        visited = set()
-        visited.add((x,y))
-        
-        directions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
-        
-        # queue가 다 빌 때까지
+    # 상하좌우에 대한 directions 도 정의 (for 문으로 모두 탐색할 거임)
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    
+    # deque 사용 -> x,y 넣어줘야하고, 그리고 거리도 넣어줘야 됨.
+    queue = deque([(0, 0, 1)])
+    
+    # 방문 기록
+    visited = [[False] * m for _ in range(n)]
+    
+    def bfs():
         while queue:
             x, y, distance = queue.popleft()
             
+            # 목적지 도착하면 끝.
             if x == n-1 and y == m-1:
                 return distance
             
             for dx, dy in directions:
                 nx, ny = x + dx, y + dy
                 
-                if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1 and (nx, ny) not in visited:
+                # 범위 확인, 길 확인, 방문 확인
+                if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1 and not visited[nx][ny]:
                     queue.append((nx, ny, distance + 1))
-                    visited.add((nx, ny))
+                    visited[nx][ny] = True
+        
         return -1
     
-    return bfs(0, 0)
+    return bfs()
