297d571e6178bc59b86373611695e76ef99ffb66
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-02-21 21:33:49 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,16 +1,13 @@
+from collections import defaultdict
+
 def solution(clothes):
-    dictionary = {i[1]: set() for i in clothes}
+    dictionary = defaultdict(int)
+    result = 1
     
-    # 각 항목을 해당하는 집합에 추가
-    for i in clothes:
-        dictionary[i[1]].add(i[0])
+    for name, kind in clothes:
+        dictionary[kind] += 1
+
+    for value in dictionary.values():
+        result *= (value + 1)
     
-    # 각 set에 착용x (none) 추가해주고, 착용x로만 조합된 케이스 -1
-    for key in dictionary.keys():
-        dictionary[key].add('none')
-    
-    temp = 1
-    for key in dictionary.keys():
-        temp *= len(dictionary[key])
-        
-    return temp - 1
+    return result - 1
