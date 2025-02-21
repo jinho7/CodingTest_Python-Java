b9975c7a6daea0c295a6376efd72a17143cf1828
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-02-21 21:22:12 +0900

    [level 2] Title: 올바른 괄호, Time: 9.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,14 +1,12 @@
 def solution(s):
     
-    # 남아 있는 '(' 개수 저장
     count = 0
-    for char in s:
-        if char == '(':
+    for i in range(len(s)):
+        if s[i] == '(':
             count += 1
-        else:  # char == ')'
-            # 스택이 비어있는데 오른쪽 괄호가 나온 경우
-            if count == 0:  
-                return False
+        else:
             count -= 1
-    # 모든 괄호가 올바르게 짝지어졌으면 0
-    return count == 0
+        
+        if count < 0:
+            return False
+    return True if count == 0 else False    
