6e7c3f054773ab8441ce9cbf1d633df5208200ce
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-04-08 15:06:50 +0900

    Title: 격자에 점 그리기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250408/격자에 점 그리기/draw-points-on-grid.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+
+coins = [tuple(map(int, input().split())) for _ in range(m)]
+
+arr = [[0 for _ in range(n)] for _ in range(n)]
+
+num = 1
+for coin in coins:
+    x, y = coin
+    arr[x-1][y-1] = num
+    num += 1
+    
+for i in range(n):
+    for j in range(n):
+        print(arr[i][j], end= ' ')
+    print()
