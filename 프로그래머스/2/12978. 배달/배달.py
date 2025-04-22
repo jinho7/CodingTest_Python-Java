981fa9a616bc48a0a2e8e45b60a84ff5717dc9be
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-04-22 15:30:37 +0900

    [level 2] Title: 배달, Time: 4.66 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,43 +1,36 @@
 import heapq
-from collections import defaultdict
 
 def solution(N, road, K):
+    answer = 0
+    # 2차원 배열로 하면 X
+    # hash에다가 길 저장
+    dic = { i : [] for i in range(N) }
+    
+    for x, y, weight in road:
+        dic[x-1].append((y-1, weight))
+        dic[y-1].append((x-1, weight))
+        
+    def dijkstra():
+        distances = [float('inf') for _ in range(N)]
+    
+        # distance, node 순 (max_heap)
+        heap = [(0, 0)]
+        distances[0] = 0
+        while heap:
+            dist, c_node = heapq.heappop(heap)
+            # 모든 노드 검색
+            for n_node, weight in dic[c_node]:
+                # 연결된 노드 거르기
+                if weight > 0 :
+                    n_dist = dist + weight
+                    if n_dist < distances[n_node]:
+                        distances[n_node] = n_dist
+                        heapq.heappush(heap, (n_dist, n_node))
+        return distances
+    
+    temp = dijkstra()
 
-    graph = defaultdict(list)
-    
-    for start, end, weight in road:
-        graph[start].append((weight, end))
-        graph[end].append((weight, start))
-        
-    print(graph)
-    
-    # 1부터 각 마을의 최소 거리
-    distances = {node : float('inf') for node in range(1, N+1)}
-    distances[1] = 0
-    
-    # 우선순위 큐에 저장 (앞에 기준임 -> (weight, 연결 node) )
-    queue = [(0, 1)]
-    # heapq.heappush(), heapq.heappop() 으로 함수로만 적으면 힙정렬이 돼서 들어감
-    # 선언은 그냥 queue 처럼
-    
-    while queue:
-        current_distance, current_node = heapq.heappop(queue)
-        
-        # 종료 or pass 조건 먼저
-        # 만약에 지금 가려는 게 저장된 것 보다 길다면 pass
-        if current_distance > distances[current_node]:
-            continue
-            
-        # 해당 노드에서 인접한 노드 모두 탐색
-        # 마치 dx, dy in direction 처럼 
-        for weight, next_node in graph[current_node]:
-            distance = current_distance + weight
-            
-            # 더 짧은 경로 발견 시 거리 테이블과 큐를 업데이트
-            if distance < distances[next_node]:
-                distances[next_node] = distance
-                heapq.heappush(queue, (distance, next_node))
-    
-    reachable_nodes = sum(1 for distance in distances.values() if distance <= K)
-        
-    return reachable_nodes
+    for x in range(len(temp)):
+        if temp[x] <= K:
+            answer += 1
+    return answer
