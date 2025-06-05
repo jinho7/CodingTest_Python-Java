e2d376314e58668ce1e5ce66794ba0840ca9c8f6
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-06-05 16:41:10 +0900

    [level 3] Title: 여행경로, Time: 0.11 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,19 +1,20 @@
 from collections import defaultdict
 
 def solution(tickets):
-    tickets.sort()  # 사전순으로 미리 정렬
-
+    tickets.sort(key=lambda x: x[1])
+    
     graph = defaultdict(list)
     for a, b in tickets:
         graph[a].append(b)
-
-    path = []
-
+    
+    print(graph)
     def dfs(country):
         while graph[country]:
+            print(graph[country])
             next_country = graph[country].pop(0)
             dfs(next_country)
         path.append(country)
-
-    dfs("ICN")
+        print(path)
+    path = []
+    dfs('ICN') 
     return path[::-1]
