a99e0231084efc8845263263ccc4676fb837800d
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-06-11 18:53:55 +0900

    [level 3] Title: 부대복귀, Time: 667.93 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.py
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.py
@@ -8,23 +8,17 @@ def solution(n, roads, sources, destination):
         graph[y].append(x)
         
     # 각 sources들에서 dsstination으로 복귀하는 최단 시간을 return 하는 bfs 함수
-    # 가 아니라, destination으로 시작해서 각 지점을 다 저장. dist 를 return
-    def shortest_path(n, start):
-        dist = [-1] * (n + 1)
-        queue = deque([(start)])
-        dist[start] = 0
+    # 가 아니라, destination으로 시작해서 각 지점을 다 저장
+    dist = [-1] * (n + 1)
+    queue = deque([(destination)])
+    dist[destination] = 0
 
-        while queue:
-            curr = queue.popleft()
-            for neighbor in graph[curr]:
-                # 처음 방문
-                if dist[neighbor] == -1:
-                    dist[neighbor] = dist[curr] + 1
-                    queue.append((neighbor))
-        return dist
+    while queue:
+        curr = queue.popleft()
+        for neighbor in graph[curr]:
+            # 처음 방문
+            if dist[neighbor] == -1:
+                dist[neighbor] = dist[curr] + 1
+                queue.append((neighbor))
     
-    dist = shortest_path(n, destination)
-    answer = []
-    for source in sources :
-        answer.append(dist[source])
-    return answer
+    return [dist[s] for s in sources]
