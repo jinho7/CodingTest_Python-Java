c9d2497b6a2ccd9ad749151582f471b04429a66e
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-06-11 18:22:32 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.28 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -2,32 +2,11 @@ import java.util.*;
 
 class Solution {
     public long solution(long n) {
-        // 1. 문자열로 변환
-        String s = String.valueOf(n);
-        // 2. char로 변환시켜서 char[]에 담아주기
-        char[] ch = s.toCharArray();
-        // 3. 내림차순 정렬 위해서 다시 Character[] (Wrapper Type)으로 바꿔줘야 함..
-        Character[] arr = new Character[ch.length];
-        for (int i = 0; i < ch.length; i++) {
-            arr[i] = ch[i];
-        }
-            
-        // // 3. 이것도 가능함! char[] → List<Character>
-        // List<Character> list = new ArrayList<>();
-        // for (char c : chars) {
-        //     list.add(c); // autoboxing 발생
-        // }
-        
-        // 4. 실제 정렬
-        Arrays.sort(arr, (a, b) -> b - a);
-        
-        // 5. 다시 String으로 변환
+        // {String, String, ... } 으로 담아주고 sb에서 reverse 쓰기
+        String[] st = String.valueOf(n).split("");
+        Arrays.sort(st);
         StringBuilder sb = new StringBuilder();
-        for (char c : arr) {
-            sb.append(c);
-        }
-        
-        long answer = Long.parseLong(sb.toString());
-        return answer;
+        for (String s : st) sb.append(s);
+        return Long.parseLong(sb.reverse().toString());
     }
 }
