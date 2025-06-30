cbadd4b9f0e302b682f92954d7df983c7a5db59d
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-06-30 17:58:17 +0900

    [level 3] Title: 순위, Time: 253.37 ms, Memory: 9.61 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,40 +1,41 @@
 from collections import defaultdict
 
+# Floyd-Warshall 방법으로도 풀어보자.
 def solution(n, results):
-    # 정확한 순위를 알 수 있는 선수의 수 = 나를 제외한 강한 사람 / 약한 사람 모두 알기
-    # = 강 / 약 의 합이 n-1
+    # 1. 초기 그래프 세팅 (문자열: 'Win', 'Lose', 'Unknown')
+    graph = [['Unknown' for _ in range(n+1)] for _ in range(n+1)]
     
-    # 1이 2를 이기고, 2가 3을 이기면 → 1은 3보다 강하다 이런건 어떻게 판단?
-    # 그래프 탐색으로 타고 들어가서 추가해주자.
-    
-    # i가 이긴 사람들
-    win_graph = defaultdict(list)
-    # i가 진 사람들
-    lose_graph = defaultdict(list)
-    
-    for l, w in results:
-        win_graph[l].append(w)
-        lose_graph[w].append(l)
-    
-    # 탐색으로 이긴 사람, 진 사람 체크하기
-    def dfs(start, graph, visited):
-        for next_player in graph[start]:
-            if next_player not in visited:
-                visited.add(next_player)
-                dfs(next_player, graph, visited)
-    
-    answer = 0
-        
     for i in range(1, n+1):
-        win_visited = set()
-        lose_visited = set()
-        
-        # 그래프 전파 및 visited 추가.
-        dfs(i, win_graph, win_visited)
-        dfs(i, lose_graph, lose_visited)
-        
-        print(win_visited, lose_visited)
-        if len(win_visited) + len(lose_visited) == n - 1:
+        graph[i][i] = 'Self'  # 자기 자신은 'Self'
+
+    for a, b in results:
+        graph[a][b] = 'Win'
+        graph[b][a] = 'Lose'
+    
+    # 초기 탐색용 그래프
+    for x in range(1, len(graph)):
+        print(graph[x][1:len(graph)])
+
+    # 플로이드-워셜 알고리즘
+    for k in range(1, n+1):  # 경유 노드
+        for i in range(1, n+1):  # 출발 노드
+            for j in range(1, n+1):  # 도착 노드
+                # i > k > j -> i 와 j 관계도 Update
+                if graph[i][k] == 'Win' and graph[k][j] == 'Win':
+                    graph[i][j] = 'Win'
+                    graph[j][i] = 'Lose'
+                elif graph[i][k] == 'Lose' and graph[k][j] == 'Lose':
+                    graph[i][j] = 'Lose'
+                    graph[j][i] = 'Win'
+
+    # 3. 순위 계산
+    answer = 0
+    for i in range(1, n+1):
+        known = 0
+        for j in range(1, n+1):
+            if graph[i][j] != 'Unknown' and graph[i][j] != 'Self':
+                known += 1
+        if known == n - 1:
             answer += 1
-        
+
     return answer
