4058b808385f6c07693dd7782f04199d6d9aa9d6
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-06-30 19:04:30 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 11.55 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/README.md
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 제일 작은 수 제거하기 - 12935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935) 
 
 ### 성능 요약
 
-메모리: 101 MB, 시간: 13.90 ms
+메모리: 109 MB, 시간: 11.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 26일 15:38:11
+2025년 06월 30일 19:04:26
 
 ### 문제 설명
 
--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -3,15 +3,20 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            list.add(arr[i]);
+        for (int x : arr) {
+            list.add(x);
         }
         int min = Collections.min(list);
         list.remove(Integer.valueOf(min));
         
-        int[] answer = list.stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
+        // int[] answer = list.stream()
+        //         .mapToInt(Integer::intValue)
+        //         .toArray();
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
         return answer.length > 0 ? answer : new int[]{-1};
     }
 }
