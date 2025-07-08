ab95750de4e11e5ef321bbda38ace08e1b0a60e7
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-07-08 14:37:42 +0900

    [level 3] Title: 입국심사, Time: 404.45 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,25 +1,15 @@
 from itertools import combinations
 
 def solution(n, times):
-    # ex1) 40 분이 걸림.
-    # 40 / 7 + 40 / 10 => 충분히 6명 처리 가능
-    # ex2) 27분이 걸림
-    # 27 / 7 + 27 / 10 -> 3명처리, 2명처리 -> 총 5명 밖에 처리 못함.
+    l = (n//len(times) + 1)*min(times)
+    r = (n//len(times) + 1)*max(times)
     
-    
-    def check_can_pass(total):
-        return sum(total // time for time in times) >= n
-    
-    left, right = 0, max(times) * n
-    print(right)
-    answer = 0
-    while left <= right:
-        mid = (left + right) // 2
-        # 통과? 일단 answer 해놓고, 더 작은수 가능한지 보기
-        if check_can_pass(mid):
-            answer = mid
-            right = mid - 1
+    while l <= r:
+        m = (l + r) // 2
+        people = sum(m // time for time in times)
+        if people >= n:
+            answer = m
+            r = m -1
         else:
-            left = mid + 1
-    
+            l = m + 1
     return answer
