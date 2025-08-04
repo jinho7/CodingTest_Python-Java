6af923fdcad33d84d0770b9856526a6055bee206
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-08-04 19:34:10 +0900

    [level 2] Title: 피보나치 수, Time: 522.52 ms, Memory: 455 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,23 +1,9 @@
-# 아으 또 런타임 에러야. HashMap? dictionary?
-
-fibo_dict = {'fibo0' : 0, 'fibo1' : 1}
-
 def solution(n):
-    # 한 번 1 부터 돌려서 딕셔너리에 저장
-    for i in range(2, n):
-        fibo(i)
-    # 실제 fibo(n) 은 사전에서 찾아와서 오래 안걸릴 듯
-    return fibo(n) % 1234567
+    dp = [0] * (n+1)
+    dp[0] = 0
+    dp[1] = 1
+    
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
 
-def fibo(n):
-    if n == 0:
-        return fibo_dict['fibo0']
-    elif n == 1:
-        return fibo_dict['fibo1']
-    else:
-        # 사전 저장
-        temp_str1 = 'fibo' + str(n)
-        temp_str2 = 'fibo' + str(n-1)
-        temp_str3 = 'fibo' + str(n-2)
-        fibo_dict[temp_str1] = fibo_dict[temp_str2] + fibo_dict[temp_str3]
-        return fibo_dict[temp_str1]
+    return dp[n] % 1234567
