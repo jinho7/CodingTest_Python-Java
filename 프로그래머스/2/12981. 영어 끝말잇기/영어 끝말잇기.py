c107d785012566cf56d6b7acb9ba878fe077a09a
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-09-08 14:08:24 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.21 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,33 +1,20 @@
-import math
-
 def solution(n, words):
     
-    used = [words[0]]
-    last_turn = 1
-    for i in range(1, len(words)) :
-        # 1. 한 글자 이상인가
-        if len(words[i]) <= 1:
-            last_turn = i+1
-            break
-        # 2. used에 있는 지 체크?
-        elif words[i] in used:
-            last_turn = i+1
-            break
-        # 3. 룰 : 끝에 말이랑 첫 말이랑 같은지
-        elif used[-1][-1] != words[i][0]:
-            last_turn = i+1
-            break
-        else :
-             # 후처리 : used에 담기
-            last_turn = i+1
-            used.append(words[i])
-            
-    if len(words) == last_turn and used[-1] == words[-1]:
-        return [0,0]
-        
-    # 가장 먼저 탈락하는 사람의 번호
-    eliminated_num = (last_turn - 1) % n + 1
-    # 탈락자가 자신의 몇 번째 차례에 탈락
-    eliminated_turn = math.ceil(last_turn / n)
+    queue = [words[0]]
 
-    return eliminated_num, eliminated_turn
+    def validation(word):
+        # 1. 한 단어인지, 2. 끝말잇기
+        print(queue[-1], word)
+        if len(word) > 1 and queue[-1][-1] == word[0]:
+            # 3. 이전에 사용한 단어인지
+            if word not in queue:
+                return True
+        return False
+    
+    for i in range(1, len(words)):
+        if validation(words[i]):
+            queue.append(words[i])
+            continue
+        else:
+            return [(i + 1) % n or n, (i + n) // n]
+    return [0, 0]
