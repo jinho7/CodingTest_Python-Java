0b30b3d0704d45c72b943c42a1486ca4a2d207c9
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-09-08 16:17:59 +0900

    [level 2] Title: 괄호 회전하기, Time: 150.98 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 137.09 ms
+메모리: 9.36 MB, 시간: 150.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 31일 17:53:12
+2025년 09월 08일 16:17:54
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,24 +1,32 @@
 def solution(s):
-    s2 = s+s
+    n = len(s)
+    
+    # [초기 검증]: 홀수면 바로 0 반환
+    if n % 2 != 0:
+        return 0
+
+    new_s = s * 2
     answer = 0
-    for i in range(len(s)):
-        if is_right(s2[i:len(s)+i]):
+    correct_dict = {')': '(', ']': '[', '}': '{'}
+    # correct_match = [['(',')'], ['[': ']'], ['{': '}']]
+    
+    def validate(x):
+        # 삭제 처리: 보관용
+        stack = []
+        
+        # find the (), [], {} and delete matched things
+        for i in x:
+            if i in correct_dict.values():
+                stack.append(i)
+            else:
+                # 스택 끝 값 동일
+                if stack and stack[-1] == correct_dict[i]:
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0
+
+    for i in range(n):
+        if validate(new_s[i:i+n]):
             answer += 1
     return answer
-
-# 참고함
-def is_right(s):
-    stack = []
-    matching = {')': '(', ']': '[', '}': '{'}
-    
-    for char in s:
-        if char in matching.values():
-            stack.append(char)
-        elif char in matching:
-            if stack and stack[-1] == matching[char]:
-                stack.pop()
-            else:
-                return False
-    return len(stack) == 0
-
-# "{(})" 케이스 해결 못하겠음
