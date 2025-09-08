3651c3fa3accbc33178f767f875c1fc11d831fed
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-09-08 14:55:33 +0900

    [level 2] Title: 할인 행사, Time: 188.43 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,27 +1,18 @@
+from collections import defaultdict
+
 def solution(want, number, discount):
+    want_amount = defaultdict(int)
+    for x, y in zip(want, number):
+        want_amount[x] = y
     
-    wanted_dict = {x : y for x,y in zip(want,number)}
-    
-    count = 0
-    for i in range(len(discount)-10 + 1):
-        #print(str(i+1) + "번째 날 ----------------")
-        temp = discount[i:i+10]
-        goods_amount_dict = { item : temp.count(item) for item in temp}
-        match = True
-        #print(goods_amount_dict)
-        for item in wanted_dict.keys():
-            #print(item + "에 대해여")
-            try :
-                if wanted_dict[item] <= goods_amount_dict[item]:
-                    #print("만족!", wanted_dict[item], goods_amount_dict[item])
-                    None
-                else :
-                    #print("적어서 불만족!", wanted_dict[item], goods_amount_dict[item])
-                    match = False
-            except KeyError:
-                #print("없어서 불만족!")
-                match = False
-                break;
-        if match == True:
-            count += 1 
-    return count
+    answer = 0
+    for i in range(len(discount) - 9):
+        count_dict = want_amount.copy()
+        
+        # validate
+        for x in discount[i:i+10]:
+            if count_dict[x] != 0:
+                count_dict[x] -= 1
+        if sum(count_dict.values()) == 0:
+            answer += 1
+    return answer
