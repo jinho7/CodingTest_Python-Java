acfb2199cc15bc590bdd5123cf7f154de7d8fa18
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-09-13 05:46:52 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.08 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,68 +1,47 @@
-import math
-
 def solution(places):
-    answer = []
-    seat_dict = {}
-    for i in range(len(places)):
-        seat_dict[i+1] = []
-    for i in range(len(places)):
-        for j in range(len(places[0])):
-            for k in range(len(places[0][0])):
-                if places[i][j][k] == 'P':
-                    seat_dict[i+1].append([j, k])
-                    
-    for i in range(len(places)):
-        # (i+1) 강의실 체크
-        print("---------", i+1, "번 째 강의실 ---------")
-        room = places[i]
-        person_seat = seat_dict[i+1]
-        manhattan_check_list = []
-        
-        if check_manhattan(room, person_seat, manhattan_check_list):
-            answer.append(1)
-        else:
-            answer.append(0)
-    return answer
+    
+    def calculate_manhattan_distance(x1, y1, x2, y2):
+        return abs(x2-x1) + abs(y2-y1)
+    
+    def check_rule(place):
+        # 1. 각 응시자 위치 저장
+        p_location = []
+        for i in range(len(place)):
+            for j in range(len(place[0])):
+                if place[i][j] == 'P' :
+                    p_location.append((i, j))
+    
+        # 2. 일대일로 체크
+        for i in range(len(p_location)-1):
+            for j in range(i+1, len(p_location)):
+                x1, y1 = p_location[i]
+                x2, y2 = p_location[j]
+                dist = calculate_manhattan_distance(x1, y1, x2, y2)
 
-def check_manhattan(room, person_seat, manhattan_check_list):
-    #print("강의실 배치 :", room)
-    #print("수강생 위치 리스트 :", person_seat)
-    for i in range(len(person_seat)):
-        for j in range(i+1, len(person_seat)):
-            first, second = person_seat[i],person_seat[j]
-            x1, y1 = first[0], first[1]
-            x2, y2 = second[0], second[1]
-            # 맨해튼 거리 1
-            if abs(x1-x2) + abs(y1-y2) == 1:
-                #print(x1,y1, "과", x2,y2, "의 맨해튼 거리 1 입니다.")
-                return False
-            
-            # 맨허튼 거리 2
-            if abs(x1-x2) + abs(y1-y2) == 2:
-                #print(x1,y1, "과", x2,y2, "의 맨해튼 거리 2 입니다.")
-                # 직선 ; 자리 사이에 파티션 존재
-                    # (x좌표 서로 같음 -> y 좌표 사이 체크)
-                if x1 == x2:
-                    if room[x1][(y1+y2)//2] == "X" :
-                        continue
+                # 2-1. 맨하튼 거리 1 있을 경우 바로 return 0
+                if dist < 2:
+                    return 0
+                # 2-2. 맨하튼 거리 2 있을 경우 추가 체크
+                elif dist == 2:
+                    # (1자 사이), (ㄱ자 사이) 2가지로 추가 검증
+                    # 2-2-1. 1자: x축 or y축
+                    if x1 == x2:
+                        if place[x1][(y1+y2)//2] != 'X':
+                            return 0
+                    elif y1 == y2:
+                        if place[(x1+x2)//2][y1] != 'X':
+                            return 0
+                    # 2-2-2. ㄱ자
                     else:
-                        return False
-                    # (y좌표 서로 같음 -> x 좌표 사이 체크)
-                elif y1 == y2:
-                    if room[(x1+x2)//2][y1] == "X" :
-                        continue
-                    else:
-                        return False
-                # 대각선 ; 양 사이에 파티션 2개 있음
+                        if place[x1][y2] != 'X' or place[x2][y1] != 'X':
+                            return 0
+                # 2-3. 맨하튼 거리 2 초과 모두 continue
                 else:
-                    if room[x1][y2] == "X" and room[x2][y1] == "X":
-                        continue
-                    else:
-                        return False
-                    
-            # 맨허튼 거리 2 이상
-            else:
-                #print(x1,y1, "과", x2,y2, "의 맨해튼 거리 2 이상 입니다.")
-                continue
-    return True
-                
+                    continue
+        # 3. 모두 통과 시 return 1
+        return 1
+
+    answer = []
+    for i in range(len(places)):
+        answer.append(check_rule(places[i]))
+    return answer
