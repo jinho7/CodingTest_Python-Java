410c29bb6d214450548b3e612be675e85a4a2663
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-09-13 00:41:19 +0900

    [level 2] Title: [3차] n진수 게임, Time: 50.12 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,34 +1,13 @@
 def solution(n, t, m, p):
     # n 진법, t result 갯수, m 참가인원, 튜브 순서 p
     answer = ''
-    answer_len = 0
-    digits = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
     
-    all_num = 0
-    full_str = '0'
-    full_str_len = 1
-    if p == 1:
-        answer += full_str
-        answer_len += 1
-    while True:
-        s = ''
-        num = all_num
-        while num != 0:
-            x = num % n
-            if x >= 10 : x = digits[x]
-            s += str(x)
-            num = num // n
-        s = s[::-1]
-        for i in range(len(s)):
-            if ((full_str_len + i + 1) - p) % m == 0:
-                answer += s[i]
-                answer_len += 1
-                if answer_len == t:
-                    break
-        if answer_len == t:
-            break
-        full_str_len += len(s)
-        full_str += s
-        all_num += 1
+    num = 17
+    s = ''
+    while num != 0:
+        x = num % n
+        s += str(x)
+        num = num // n
+    print(s)
     
     return answer
