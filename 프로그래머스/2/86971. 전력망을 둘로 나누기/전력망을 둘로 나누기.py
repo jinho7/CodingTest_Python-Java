91a61f4208572bb8aa4701fbf93de3bc1102c8ae
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-09-13 04:06:53 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.70 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -2,39 +2,22 @@ from collections import defaultdict
 import math
 
 def solution(n, wires):
-    def find_all_nodes(wires, canceled_edge):
-        a, b = canceled_edge
-        
-        edge_dict = defaultdict(list)
-        
-        for x, y in wires:
-            if (x, y) == (a, b):
-                continue
-            edge_dict[x].append(y)
-            edge_dict[y].append(x)
-
-        def dfs(x):
-            cnt = 1
-            for i in edge_dict[x]:
-                if i not in visited:
-                    visited.add(i)
-                    cnt += dfs(i)
-            return cnt
-        
-        
-        visited = set()
-        visited.add(a)
-        count_a = dfs(a)
-
-        visited = set()
-        visited.add(b)
-        count_b = dfs(b)
-
-        return abs(count_a - count_b)
     
+    edge_dict = defaultdict(list)
+    for x, y in wires:
+        edge_dict[x].append(y)
+        edge_dict[y].append(x)
+
+    def dfs(u, visited):
+        cnt = 1
+        for v in edge_dict[u]:
+            if v not in visited:
+                visited.add(v)
+                cnt += dfs(v, visited)
+        return cnt
+
     answer = n
-    for i in range(len(wires)):
-        nodes_abs = find_all_nodes(wires, wires[i])
-        if nodes_abs < answer:
-            answer = nodes_abs
+    for x, y in wires:
+        visited = {x, y}
+        answer = min(abs(n - 2 * dfs(x, visited)), answer)
     return answer
