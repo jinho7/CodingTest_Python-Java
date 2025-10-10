3296e1fbd0d05333f8c95bb5d242c88e05b72033
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-10-10 16:33:26 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=python3) 
 
 ### 성능 요약
 
-메모리: 88.3 MB, 시간: 0.17 ms
+메모리: 9.36 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 02일 17:59:07
+2025년 10월 10일 16:33:22
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,34 +1,14 @@
-num_dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
-
 def solution(s):
-    answer = s
-    for key, value in num_dic.items():
-        answer = answer.replace(key, value)
-    return int(answer)
-
-
-
-
-def my_solution(s):
-    dic = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
     
-    answer = ''
-    for i in range(len(s)):
-        if s[i].isdigit():
-            answer += s[i]
-            i += 1
-        elif s[i].isalpha():
-            for length in range(3, 6):  # 영어 단어의 길이는 3에서 5까지의 범위로 가정
-                word = s[i:i+length]
-                if word in dic:
-                    answer += dic[word]
-                    i += length
-                    break
-    return int(answer)
+    s = s.replace('zero', '0')
+    s = s.replace('one', '1')
+    s = s.replace('two', '2')
+    s = s.replace('three', '3')
+    s = s.replace('four', '4')
+    s = s.replace('five', '5')
+    s = s.replace('six', '6')
+    s = s.replace('seven', '7')
+    s = s.replace('eight', '8')
+    s = s.replace('nine', '9')
 
-def get_value_by_key(d, key):
-    if key in d:
-        return d[key]
-    else:
-        return None
-    
+    return int(s)
