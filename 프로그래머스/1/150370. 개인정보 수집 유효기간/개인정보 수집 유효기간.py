bb39ba217301b765b11a32c9e283ea5695fb34ca
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-10-10 16:53:39 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.23 ms, Memory: 9.44 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,26 +1,23 @@
 def solution(today, terms, privacies):
-    # dic {약관 종류 : 유효기간}
-    terms_dict = {term.split()[0]: int(term.split()[1]) for term in terms}
-
-    answer = []
-    for index, item in enumerate(privacies):
-        # 해당 약관 종류 = item.split()[1]
-        if item.split()[1] in terms_dict:
-            # 해당 약관 종류가 있다면, 그 key의 value를 찾아옴 = terms_dict[item.split()[1]]
-            
-            # 개인정보 수집 일자 + 약관종류의 유효기간 * 28 보다 오늘 일자가 더 크면
-            if replace(item.split()[0]) + terms_dict[item.split()[1]] * 28 <= replace(today):
-                answer.append(index+1)
-            
-    return answer
-
-# 'yyyy.mm.dd' 형식의 stirng -> 몇 일인지 치환하는 코드
-def replace(yyyymmdd):
-    result = 0
-    # (년-1)x12x28
-    result += (int(yyyymmdd.split('.')[0])-1) * 12 * 28
-    # (월-1)x28
-    result += (int(yyyymmdd.split('.')[1])-1) * 28
-    result += int(yyyymmdd.split('.')[2]) 
     
-    return result
+    term_dict = {}
+    for x in terms:
+        term, month = x.split()
+        # -1 일 까지! 유효
+        term_dict[term] = int(month) * 28 - 1
+    
+    # 날짜 -> 일로 변경 (2000 제외하고 뒤만)
+    def date_to_days(date):
+        year, month, day = date.split('.')
+        # month = (int(year) - 2001) * 12 + int(month)
+        return ((int(year) - 2001) * 12 + int(month) - 1) * 28 + int(day)
+    
+    today_day = date_to_days(today)
+    answer = []
+    
+    for idx, privacy in enumerate(privacies):
+        date, term = privacy.split()
+        if date_to_days(date) + term_dict[term] < today_day:
+            answer.append(idx+1)
+        
+    return answer
