8ac4c267997cf13a2a232c61e1b9a0ffdb381df1
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-10-10 17:16:15 +0900

    [level 3] Title: 네트워크, Time: 2.80 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,20 @@
+from collections import deque
+
 # dfs bfs 탐색 기초 다 잡기
 def solution(n, computers):
     
-    def dfs(cur_node, visited):
-        visited.add(cur_node)
-        
-        for nxt_node in range(n):
-            if computers[cur_node][nxt_node] == 1 and nxt_node not in visited:
-                dfs(nxt_node, visited)
-    
     visited = set()
     answer = 0
-    
+
     for node in range(n):
         if node not in visited:
-            dfs(node, visited)
+            q = deque([node])
+            while q:
+                cur_node = q.popleft()
+                visited.add(cur_node)
+
+                for nxt_node in range(n):
+                    if computers[cur_node][nxt_node] == 1 and nxt_node not in visited:
+                        q.append(nxt_node)
             answer += 1
-        
     return answer
